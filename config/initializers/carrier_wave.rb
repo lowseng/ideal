3858b20d40a8d4684692e7ca3e7549c0fd8532d4
lowseng <alowseng@gmail.com>
2016-03-25 02:51:42 +0000

    carrier_wave1

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,9 +2,9 @@ if Rails.env.production?
   CarrierWave.configure do |config|
     config.fog_credentials = {
     :provider => 'AWS',
-    :aws_access_key_id => ENV['AKIAJ4YARQXFSIZDXKOQ'],
-    :aws_secret_access_key => ENV['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']
+    :aws_access_key_id => ENV['S3_ACCESS_KEY'],
+    :aws_secret_access_key => ENV['S3_SECRET_KEY']
     }
-    config.fog_directory = ENV['windaqimages1']
+    config.fog_directory = ENV['S3_BUCKET']
   end
 end
