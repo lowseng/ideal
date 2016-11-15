e54fb556a6390b0d5d9984eeee9541e24b7f2545
lowseng <alowseng@gmail.com>
2016-11-15 07:15:05 +0000

    test store_dir

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -3,7 +3,7 @@
 class PictureUploader < CarrierWave::Uploader::Base
   
   include CarrierWave::MiniMagick
-  process resize_to_limit: [300, 300]
+  process resize_to_limit: [400, 300]
 
   # Include RMagick or MiniMagick support:
   # include CarrierWave::RMagick
@@ -20,7 +20,8 @@ class PictureUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "images/#{model.class.to_s.underscore}"
+    #"uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
